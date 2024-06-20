5dc54617cdbdd2bb821cafd15acfdceabf1bd47d
svhl <145035644+svhl@users.noreply.github.com>
2024-06-20 20:14:18 +0530

    added day 10 progs

--- day-10/prog10.c
+++ day-10/prog10.c
@@ -5,7 +5,8 @@
 
 void main()
 {
-	int i, n, head, ch, res, tot = 0;
+	int i, j, n, head, c, temp, ch, res, tot = 0;
+	char d;
 	printf("Enter no. of requests:\n");
 	scanf("%d", &n);
 	int req[n];
@@ -42,11 +43,193 @@ void main()
 			}
 
 			tot += res;
-
 			printf("\n%d -> %d = %d", head, req[i], res);
-			
 			head = req[i];
 		}
+	}
+
+	else if(ch == 2)
+	{	
+		printf("Enter size of cylinder:\n");
+		scanf("%d", &c);
+		printf("Enter direction (r/l):\n");
+		scanf("%c", &d);
+		scanf("%c", &d);
+		temp = head;
+
+		if(d == 'r')
+		{	
+			for(i = head; i < c; i++)
+			{
+				for(j = 0; j < n; j++)
+				{
+					if(req[j] == i)
+					{
+						res = req[j] - temp;
+						tot += res;
+						printf("\n%d -> %d = %d", temp, req[j], res);
+						temp = req[j];
+					}
+				}
+			}
+
+			res = c - 1 - temp;
+			tot += res;
+			printf("\n%d -> %d = %d", temp, c - 1, res);
+			temp = c - 1;
+
+			for(i = head; i > 0; i--)
+			{
+				for(j = 0; j < n; j++)
+				{
+					if(req[j] == i)
+					{
+						res = temp - req[j];
+						tot += res;
+						printf("\n%d -> %d = %d", temp, req[j], res);
+						temp = req[j];
+					}
+				}
+			}
+		}
+
+		else if(d == 'l')
+		{
+			for(i = head; i > 0; i--)
+			{
+				for(j = 0; j < n; j++)
+				{
+					if(req[j] == i)
+					{
+						res = temp - req[j];
+						tot += res;
+						printf("\n%d -> %d = %d", temp, req[j], res);
+						temp = req[j];
+					}
+				}
+			}
+
+			tot += temp;
+			printf("\n%d -> 0 = %d", temp, temp);
+			temp = 0;
+
+			for(i = head; i < c; i++)
+			{
+				for(j = 0; j < n; j++)
+				{
+					if(req[j] == i)
+					{
+						res = req[j] - temp;
+						tot += res;
+						printf("\n%d -> %d = %d", temp, req[j], res);
+						temp = req[j];
+					}
+				}
+			}
+		}
+
+		else
+		{
+			printf("Invalid direction\n");
+		}
+	}
+
+	else if(ch == 3)
+	{
+		printf("Enter size of cylinder:\n");
+		scanf("%d", &c);
+		printf("Enter direction (r/l):\n");
+		scanf("%c", &d);
+		scanf("%c", &d);
+		temp = head;
+
+		if(d == 'r')
+		{
+			for(i = head; i < c; i++)
+			{
+				for(j = 0; j < n; j++)
+				{
+					if(req[j] == i)
+					{
+						res = req[j] - temp;
+						tot += res;
+						printf("\n%d -> %d = %d", temp, req[j], res);
+						temp = req[j];
+					}
+				}
+			}
+
+			res = c - 1 - temp;
+			tot += res;
+			printf("\n%d -> %d = %d", temp, c - 1, res);
+			res = c - 1;
+			tot += res;
+			printf("\n%d -> 0 = %d", c - 1, res);
+			temp = 0;
+
+			for(i = 0; i < head; i++)
+			{
+				for(j = 0; j < n; j++)
+				{
+					if(req[j] == i)
+					{
+						res = req[j] - temp;
+						tot += res;
+						printf("\n%d -> %d = %d", temp, req[j], res);
+						temp = req[j];
+					}
+				}
+			}
+		}
+
+		else if(d == 'l')
+		{
+			for(i = head; i > 0; i--)
+			{
+				for(j = 0; j < n; j++)
+				{
+					if(req[j] == i)
+					{
+						res = temp - req[j];
+						tot += res;
+						printf("\n%d -> %d = %d", temp, req[j], res);
+						temp = req[j];
+					}
+				}
+			}
+
+			tot += temp;
+			printf("\n%d -> 0 = %d", temp, res);
+			res = c - 1;
+			tot += res;
+			printf("\n0 -> %d = %d", c - 1, res);
+			temp = c - 1;
+
+			for(i = c - 1; i > head; i--)
+			{
+				for(j = 0; j < n; j++)
+				{
+					if(req[j] == i)
+					{
+						res = temp - req[j];
+						tot += res;
+						printf("\n%d -> %d = %d", temp, req[j], res);
+						temp = req[j];
+					}
+				}
+			}
+		}
+
+		else
+		{
+			printf("Invalid direction\n");
+		}
+	}
+
+	else
+	{
+		printf("Wrong choice\n");
+		return;
 	}
 
 	printf("\n\nTotal seek operations = %d\n", tot);
